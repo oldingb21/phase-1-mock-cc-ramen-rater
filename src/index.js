301632d3cafab10280c1d5308fdf875d40ca79d7
Brenden Olding <oldingb21@gmail.com>
2023-05-25 02:38:24 -0400

    lab complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
 // write your code here
+window.addEventListener('DOMContentLoaded', (e) => {
+    fetch("http://localhost:3000/ramens")
+    .then((res) => res.json())
+    .then((ramens) => {
+        ramens.forEach(ramen => {
+            const ramenImg = document.createElement('img');
+            ramenImg.src = ramen.image
+            document.getElementById("ramen-menu").appendChild(ramenImg);
+            ramenImg.addEventListener('click', e => {
+                const ramenDetailImg = document.querySelector('.detail-image');
+                ramenDetailImg.src = e.target.src;
+                const ramenDetailName = document.querySelector('.name');
+                ramenDetailName.textContent = ramen.name
+                const ramenDetailRestaurant = document.querySelector('.restaurant');
+                ramenDetailRestaurant.textContent = ramen.restaurant;
+                const ramenRating = document.querySelector('#rating-display');
+                ramenRating.textContent = ramen.rating;
+                const ramenComment = document.querySelector('#comment-display');
+                ramenComment.textContent = ramen.comment;
+            })
+        });
+    })
+    document.querySelector('#new-ramen').addEventListener('submit', e => {
+        e.preventDefault();
+        const newImg = document.createElement('img')
+        newImg.src = e.target.image.value;
+        fetch("http://localhost:3000/ramens", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify({
+                "name":`${e.target.name.value}`,
+                "restaurant":`${e.target.restaurant.value}`,
+                "image":`${e.target.image.value}`,
+                "rating":`${e.target.rating.value}`,
+                "comment":`${e.target.new_comment.value}`,
+            })
+        })
+        document.getElementById("ramen-menu").appendChild(newImg);
+    })
+})
